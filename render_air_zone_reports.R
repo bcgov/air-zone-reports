294c188a62b917a484fff445cd4edbf7fe2344b7
Kara Woo <woo.kara@gmail.com>
2019-03-07 21:50:28 -0800

    Use RData files instead of running analyses from scratch

--- render_air_zone_reports.R
+++ render_air_zone_reports.R
@@ -2,30 +2,18 @@
 ####  Render reports  ####
 ##########################
 
-library("here")
 library("purrr")
 library("rmarkdown")
 
-## Run the ozone and PM2.5 analyses to generate the necessary objects. These
-## paths will need to be replaced with the correct location where the analyses
-## will be run.
+## Load the necessary objects from the ozone and PM2.5 analyses
 ozone_dir <- "../ozone-caaqs-indicator/"
 pm25_dir  <- "../pm25-caaqs-indicator/"
 
-## Files from each analysis to run. We don't need all the saved outputs from
-## these analyses, just the R objects, so this is a bit faster.
-files_to_run <- c("01_load.R", "02_clean.R", "03_analysis.R")
+load(file.path(ozone_dir, "tmp/ozone_clean.RData"))
+load(file.path(ozone_dir, "tmp/analysed.RData"))
 
-## Need to run analyses in their own directories because they depend on files
-## created within those directories during the course of the analysis
-setwd(ozone_dir)
-walk(files_to_run, source)
-
-setwd(pm25_dir)
-walk(files_to_run, source)
-
-## Now return to air zone reports directory to render files
-setwd(here())
+load(file.path(pm25_dir, "tmp/pm25_clean.rda"))
+load(file.path(pm25_dir, "tmp/analysed.RData"))
 
 ## Render all reports, named 01_northeast.Rmd, 02_central_interior.Rmd, etc.
 ## using default parameters
