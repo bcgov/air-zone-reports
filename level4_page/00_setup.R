fe7ec6c93846cd37160566fda99f68d375c2f49d
Robles <Jerome.Robles@gov.bc.ca>
2023-01-30 02:24:46 -0800

    added emissions and trends functions

---
+++ level4_page/00_setup.R
@@ -0,0 +1,158 @@
+#' CREATE TRENDS of air quality in BC
+#' 
+#' @param dirs_location is the location of the data files
+get_trends <- function(dirs_location = './data/out') {
+library(dplyr)
+library(readr)
+library(ggplot2)
+library(plotly)
+
+# dirs_location <- './data/out'  #local location, two dots for final, one dot for debug
+if (0) {
+  dirs_location <- './data/out'
+}
+
+list.files(dirs_location)
+
+df_data_trends_caaqs <- readr::read_csv(paste(dirs_location,'caaqs_results.csv',sep='/')) 
+df_data_trends_annual <- readr::read_csv(paste(dirs_location,'annual_results.csv',sep='/')) %>%
+  filter(!is.na(value),value>-10)
+
+maxyear <- max(df_data_trends_caaqs$year)
+df_stations <- readr::read_csv(paste(dirs_location,'liststations.csv',sep='/')) %>%
+  mutate(AQMS = ifelse(is.na(AQMS),'N/A',AQMS)) %>%
+  filter(AQMS != 'N') %>%
+  filter(site %in% df_data_trends_annual$site)
+
+colnames(df_stations)
+
+df_plot_metric <- df_data_trends_annual%>%
+  select(metric,parameter) %>%
+  distinct()
+
+#rolling 3-year average needed for
+#pm25 annual
+#pm25 24-hour
+#o3_8hr
+#no2 1-hour
+#so2 1-hour
+
+#calculate 3-year running average
+df_data_trends_annual_3yr <- df_data_trends_annual %>%
+  mutate(index = 1:n())
+df_data_trends_annual_3yr_ <- NULL
+for (i in 0:2) {
+  df_data_trends_annual_3yr_ <- df_data_trends_annual_3yr_ %>%
+    bind_rows(
+      df_data_trends_annual_3yr %>%
+        mutate(year=year+i)
+      
+      
+    )
+}
+
+#those with metric "MEAN_1HR" are averaged over 1-year
+#all the rest are averaged over 3 years
+df_data_trends_annual_3yr <- df_data_trends_annual_3yr_ %>%
+  mutate(valid_count = ifelse(is.na(value),0,1)) %>%
+  group_by(parameter,site,instrument,tfee,year,metric) %>%
+  dplyr::mutate(value_3yr = sum(value,na.rm = TRUE),valid_n =sum(valid_count)) %>%
+  filter(valid_n>=2) %>%
+  ungroup() %>%
+  mutate(value = ifelse(grepl('MEAN_1HR',metric,ignore.case = TRUE),value,value_3yr/valid_n)) %>%
+  select(-value_3yr,-valid_count) %>%
+  filter(year<=maxyear)
+
+
+#summarize for air zone plot
+df_data_trends_annual_airzone <- df_data_trends_annual_3yr %>%
+  left_join(df_stations %>%
+              select(site,AIRZONE)) %>%
+  filter(!is.na(AIRZONE)) %>%
+  group_by(parameter,tfee,year,metric,AIRZONE) %>%
+  dplyr::summarise(value_avg = mean(value,na.rm = TRUE),
+                   value_min = min(value,na.rm = TRUE),
+                   value_max = max(value,na.rm = TRUE))
+
+df_data_trends_annual_overall <- df_data_trends_annual_3yr %>%
+  left_join(df_stations %>%
+              select(site,AIRZONE)) %>%
+  filter(!is.na(AIRZONE)) %>%
+  group_by(parameter,tfee,year,metric) %>%
+  dplyr::summarise(value_avg = mean(value,na.rm = TRUE),
+                   value_min = min(value,na.rm = TRUE),
+                   value_max = max(value,na.rm = TRUE)) %>%
+  mutate(AIRZONE = 'BC')
+
+df_data_trends_annual_airzone <- df_data_trends_annual_airzone %>%
+  bind_rows(df_data_trends_annual_overall) %>%
+  filter(!tfee) %>%
+  filter(grepl('RAW',metric,ignore.case = TRUE))
+
+#create reference years
+#this shows how many percent increase or decrease in value
+result_table <- df_BC_summary_ref<- df_data_trends_annual_airzone %>%
+  filter(year %in% c(1990,2000,2010,maxyear)) %>%
+  select(parameter,year,metric,AIRZONE,value_avg) %>%
+  tidyr::pivot_wider(names_from = year, values_from = value_avg) %>%
+  mutate(perc_2000 = envair::round2((`2021`-`2000`)/`2000`*100))
+
+#plot
+lst_parameters <- df_data_trends_annual_airzone %>%
+  filter(!tfee) %>%
+  filter(AIRZONE == 'BC') %>%
+  filter(year >=1990) %>%
+  mutate(parameter_label = paste(parameter,metric)) %>%
+  pull(parameter_label) %>%
+  unique()
+paste(lst_parameters,collapse=',')
+#define parameters for recoding
+df_parameters <- tribble(
+  ~parameter_label,~label,~CAAQS,~order,
+  'NO2 RAW_ANNUAL_98P_D1HM','NO2 (1-Hour)',60,5,
+  'NO2 RAW_ANNUAL_MEAN_1HR','NO2 (Annual)',17,4,
+  'O3 RAW_ANNUAL_4TH_D8HM','O3 (8-Hour)',62,3,
+  'PM25 RAW_ANNUAL_98P_24h','PM2.5 (24-Hour)',27,2,
+  'PM25 RAW_ANNUAL_MEAN_24h','PM2.5 (Annual)',8.8,1,
+  'SO2 RAW_ANNUAL_99P_D1HM','SO2 (1-Hour)',70,7,
+  'SO2 RAW_ANNUAL_MEAN_1HR','SO2 (Annual)',5,6
+)
+
+a <- df_data_trends_annual_airzone %>%
+  filter(!tfee) %>%
+  filter(AIRZONE == 'BC') %>%
+  filter(year >=1990) %>%
+  mutate(parameter_label = paste(parameter,metric)) %>%
+  left_join(df_parameters) %>%
+  mutate(percentAbove = (value_avg - CAAQS)/CAAQS *100) %>%
+  ungroup() 
+  
+result_ggplot <- a %>%
+ggplot(aes(x=year,y=percentAbove,colour = reorder(label,order))) +
+  geom_line() +
+  geom_hline(yintercept = 0, colour='red',linetype = 'dashed') +
+  annotate("text",x=2010, y=10,label = 'Current CAAQS') +
+  theme(legend.position = 'bottom', legend.title = element_blank(),
+        legend.key = element_blank(),
+        panel.background = element_rect(fill=NA,colour = 'black'),
+        axis.title.x = element_blank()) +
+  ylab('Percent Above/Below Current CAAQS')
+  
+result_plotly <- a %>%
+  mutate(percentAbove = envair::round2(percentAbove,n=1)) %>%
+  mutate(hovertext = paste(percentAbove,'%',sep='')) %>%
+  
+plotly::plot_ly(x=~year,y=~percentAbove,color =~reorder(label,order),
+                type='scatter',mode='lines+markers',showlegend =T,
+                hoverinfo ='y',
+                hovertemplate = paste('%{y:.1f}','%',sep='')
+                
+                ) %>%
+  layout(legend = list(orientation = 'h'),
+         yaxis = list(title = 'Percent Above/Below CAAQS'),
+         xaxis = list(title = 'Annual Reporting Period')
+  ) %>%
+  plotly::layout(hovermode = 'x unified')
+
+return(list(table = result_table,ggplot = result_ggplot,plotly = result_plotly))
+}
---
+++ level4_page/01_setup.R
@@ -0,0 +1,133 @@
+get_emissions <- function(dirs_location = './data/out'){
+library(dplyr)
+library(readr)
+library(ggplot2)
+library(plotly)
+
+# dirs_location <- './data/out'  #local location, two dots for final, one dot for debug
+if (0) {
+  dirs_location <- './data/out'
+}
+
+list.files(dirs_location)
+
+
+df_apei <- read_csv(paste(dirs_location,'EN_APEI-Can-Prov_Terr.csv',sep='/')) %>%
+  filter(Region == 'BC')
+
+#find the columns for the pollutants
+cols_apei <- colnames(df_apei)
+cols_select <- c('PM25','NOx','SOx','VOC','PM10','TPM','Pb')
+df_cols_select <- NULL
+for (cols_ in cols_select) {
+  cols_select_ <- cols_apei[grepl(cols_,cols_apei,ignore.case = TRUE)]
+  if (length(cols_select_)>0) {
+    df_cols_select <- df_cols_select %>%
+      bind_rows(tibble(
+        column_name_new = cols_,
+        column_name_old = cols_select_[1]
+      ))
+    colnames(df_apei)[colnames(df_apei) == cols_select_[1]] <- cols_
+  }
+}
+cols_select <- df_cols_select$column_name_new
+cols_apei <- colnames(df_apei)
+sectors <- unique(df_apei$Sector)
+subsectors <- unique(df_apei$Subsector)
+source <- unique(df_apei$Source)
+
+a <- df_apei %>%
+  filter(tolower(`Is it a total`) == 'y') %>%
+  filter(is.na(Sector)) %>%
+  select_at(c('Source','Year',cols_select)) %>%
+  tidyr::pivot_longer(cols = -c(Source,Year)) 
+
+cols_select <- unlist(cols_select)
+result_bar_stack <- NULL
+for (pollutant in cols_select) {
+  print(pollutant)
+  result_bar_stack_ <- {
+    a %>%
+      filter(Source != 'GRAND TOTAL') %>%
+      filter(name == pollutant) %>%
+      plotly::plot_ly(x=~Year,y=~value,color =~Source,
+                      type = 'bar',fill = ~Source) %>%
+      layout(barmode = 'stack') %>%
+    layout(legend = list(orientation = 'h'),
+           yaxis = list(title = paste(pollutant,'(tonnes/year)')),
+           xaxis = list(title = 'Year')
+    )
+  }
+  
+  if (pollutant == 'PM25') {
+    result_bar_stackpm25 <- result_bar_stack_
+  }
+  if (pollutant == 'NOx') {
+    result_bar_stacknox <- result_bar_stack_
+  }
+  if (pollutant == 'SOx') {
+    result_bar_stacksox <- result_bar_stack_
+  }
+  if (pollutant == 'VOC') {
+    result_bar_stackvoc <- result_bar_stack_
+  }
+  if (pollutant == 'PM10') {
+    result_bar_stackpm10 <- result_bar_stack_
+  }
+  if (pollutant == 'TPM') {
+    result_bar_stacktpm <- result_bar_stack_
+  }
+  if (pollutant == 'Pb') {
+    result_bar_stackpb <- result_bar_stack_
+  }
+  
+}
+
+result_bar_stack <- list(PM25 = result_bar_stackpm25,PM10 = result_bar_stackpm10,
+                         SOx = result_bar_stacksox, NOx = result_bar_stacknox,
+                         VOC = result_bar_stackvoc, TPM = result_bar_stacktpm,
+                         Pb = result_bar_stackpb)
+
+df_inventory_order <- tribble(
+  ~name,~order,
+  "PM25",1,
+  "NOx" ,2,
+  "SOx",3,
+  "VOC",4,
+  "PM10",5,
+  "TPM",6,
+  "Pb",7,
+  'TPM',8
+)
+#make line for all
+result_totals <- a %>%
+  filter(Source == 'GRAND TOTAL') %>%
+  filter(!name %in% c('TPM')) %>%
+  left_join(df_inventory_order) %>%
+plotly::plot_ly(x=~Year,y=~value,color =~reorder(name,order),
+                type='scatter',mode='lines+markers',showlegend =T,
+                hoverinfo ='y',
+                hovertemplate = paste('%{y:,.0f}',sep='')
+                
+) %>%
+  layout(legend = list(orientation = 'h'),
+         yaxis = list(title = 'Tonnes per Year'),
+         xaxis = list(title = 'Inventory Reporting Year')
+  ) %>%
+  plotly::layout(hovermode = 'x unified')
+
+
+#create summary of comparison
+result_table <- a %>%
+  filter(Year %in% c(1990,2000,2010,max(a$Year))) %>%
+  filter(!is.na(value)) %>%
+  arrange(Source,Year,name) %>%
+  tidyr::pivot_wider(names_from = Year, values_from = value) %>%
+  mutate(ref_1990 = 100 *-(`1990` - `2020`)/`1990`) %>%
+  mutate(ref_2000 = 100 *-(`2000` - `2020`)/`2000`) %>%
+  mutate(ref_2010 = 100 *-(`2010` - `2020`)/`2010`) 
+
+result <- list(totals = result_totals, pollutants = result_bar_stack, table =result_table)
+
+return(result)
+}
