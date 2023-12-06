b711160788369af4f7f0105aa0ef2b0f38a972a8
Robles <Jerome.Robles@gov.bc.ca>
2023-12-06 12:20:36 -0800

    data update

--- assets/01_load_graphics.R
+++ assets/01_load_graphics.R
@@ -12,12 +12,3 @@ saveRDS(management,'./data/out/managementsummary_tables.Rds')
 caaqs_bargraph <- plot_bar_caaqs_complete()
 saveRDS(caaqs_bargraph,'./data/out/caaqs_bargraph.Rds')
 
-
-load_data('')
-
-
-# -fix for Coastal air zone
-lst_management <- names(management)
-lst_management_2013 <- lst_management[grepl('2013',lst_management)]
-
-management[['graph_2013']]
